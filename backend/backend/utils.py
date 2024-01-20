f1443f88302a04547a48454c6f044dfa10513c10
kst164 <kartheek@tammana.org>
2024-01-20 12:43:58 +0530

    backend template part 2

---
+++ backend/backend/utils.py
@@ -0,0 +1,20 @@
+import os
+
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE = os.getenv("DATABASE")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASS = os.getenv("POSTGRES_PASS")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
+conn = psycopg2.connect(
+    database=DATABASE,
+    user=POSTGRES_USER,
+    password=POSTGRES_PASS,
+    host=POSTGRES_HOST,
+    port=POSTGRES_PORT,
+)
