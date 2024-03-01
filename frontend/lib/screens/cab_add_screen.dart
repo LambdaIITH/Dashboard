d96616f2726a65ad746f5134472d0283db0defc7
ajaykrshna <ajaykfan@gmail.com>
2024-03-02 01:14:46 +0530

    Add cab add screen

---
+++ frontend/lib/screens/cab_add_screen.dart
@@ -0,0 +1,442 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/screens/cab_add_success.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CabAddScreen extends StatefulWidget {
+  const CabAddScreen({Key? key}) : super(key: key);
+  @override
+  State<CabAddScreen> createState() => _CabAddScreenState();
+}
+
+class _CabAddScreenState extends State<CabAddScreen> {
+  DateTime selectedDate = DateTime.now();
+  String? selectedOption;
+  String? selectedOption2;
+  TimeOfDay selectedStartTime = TimeOfDay.now();
+  TimeOfDay selectedEndTime = TimeOfDay.now();
+  String seats = '';
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  Future<void> _selectStartTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedStartTime,
+    );
+    if (picked != null && picked != selectedStartTime) {
+      setState(() {
+        selectedStartTime = picked;
+      });
+    }
+  }
+
+  Future<void> _selectEndTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedEndTime,
+    );
+    if (picked != null && picked != selectedEndTime) {
+      setState(() {
+        selectedEndTime = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add a Cab',
+            style: GoogleFonts.inter(
+              fontSize: 28,
+              fontWeight: FontWeight.w700,
+              color: Colors.black,
+            )),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+            size: 30.0,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 22.0,
+          vertical: 16.0,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: const [
+                        BoxShadow(
+                          color:
+                              Color.fromRGBO(51, 51, 51, 0.10), // Shadow color
+                          offset: Offset(0, 4), // Offset in the x, y direction
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                    ),
+                    child: DropdownButtonFormField<String>(
+                      borderRadius: BorderRadius.circular(10.0),
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 15.0,
+                          vertical: 25.0,
+                        ),
+                      ),
+                      items:
+                          ['Campus', 'RGIA', 'Airport'].map((String location) {
+                        return DropdownMenuItem<String>(
+                          value: location,
+                          child: Text(
+                            location,
+                            style: GoogleFonts.inter(
+                              fontSize: 17,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? value) {
+                        selectedOption = value;
+                      },
+                      hint: selectedOption == null
+                          ? Text(
+                              'From',
+                              style: GoogleFonts.inter(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                                color: const Color(0xffADADAD),
+                              ),
+                            )
+                          : null,
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Icon(Icons.arrow_forward,
+                      size: 25.0, color: Color(0xffADADAD)),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: const [
+                        BoxShadow(
+                          color:
+                              Color.fromRGBO(51, 51, 51, 0.10), // Shadow color
+                          // Shadow color
+                          offset: Offset(0, 8), // Offset in the x, y direction
+                          blurRadius: 21.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                    ),
+                    child: DropdownButtonFormField<String>(
+                      borderRadius: BorderRadius.circular(10.0),
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 15.0,
+                          vertical: 25.0,
+                        ),
+                      ),
+                      items: [
+                        'Campus',
+                        'RGIA',
+                        'Airport',
+                      ].map((String location) {
+                        return DropdownMenuItem<String>(
+                          value: location,
+                          child: Text(
+                            location,
+                            style: GoogleFonts.inter(
+                              fontSize: 17,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? value) {
+                        selectedOption2 = value;
+                      },
+                      hint: selectedOption2 == null
+                          ? Text(
+                              'To',
+                              style: GoogleFonts.inter(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                                color: const Color(0xffADADAD),
+                              ),
+                            )
+                          : null,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 25.0),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: const [
+                        BoxShadow(
+                          color:
+                              Color.fromRGBO(51, 51, 51, 0.10), // Shadow color
+                          // Shadow color
+                          offset: Offset(0, 8), // Offset in the x, y direction
+                          blurRadius: 21.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white,
+                    ),
+                    child: TextFormField(
+                      readOnly: true,
+                      style: GoogleFonts.inter(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: selectedOption == null
+                            ? 'Date'
+                            : "${selectedDate.toLocal()}".split(' ')[0],
+                        hintStyle: GoogleFonts.inter(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: const Color(0xffADADAD),
+                        ),
+                        border: InputBorder.none,
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 20.0,
+                          vertical: 20.0,
+                        ),
+                      ),
+                      onTap: () => _selectDate(context),
+                      controller: TextEditingController(
+                        text: "${selectedDate.toLocal()}".split(' ')[0],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 25.0),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: const [
+                        BoxShadow(
+                          color:
+                              Color.fromRGBO(51, 51, 51, 0.10), // Shadow color
+                          // Shadow color
+                          offset: Offset(0, 8), // Offset in the x, y direction
+                          blurRadius: 21.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white,
+                    ),
+                    child: TextFormField(
+                      readOnly: true,
+                      style: GoogleFonts.inter(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                      ),
+                      decoration: const InputDecoration(
+                        hintText: 'Start Time',
+                        hintStyle: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xffADADAD),
+                        ),
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 20.0,
+                          vertical: 20.0,
+                        ),
+                      ),
+                      onTap: () => _selectStartTime(context),
+                      controller: TextEditingController(
+                        text:
+                            "${selectedStartTime.hour}:${selectedStartTime.minute} ",
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20.0),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: const [
+                        BoxShadow(
+                          color:
+                              Color.fromRGBO(51, 51, 51, 0.10), // Shadow color
+                          // Shadow color
+                          offset: Offset(0, 8), // Offset in the x, y direction
+                          blurRadius: 21.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white,
+                    ),
+                    child: TextFormField(
+                      readOnly: true,
+                      style: GoogleFonts.inter(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                      ),
+                      decoration: const InputDecoration(
+                        hintText: 'End Time',
+                        hintStyle: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xffADADAD),
+                        ),
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 20.0,
+                          vertical: 20.0,
+                        ),
+                      ),
+                      onTap: () => _selectEndTime(context),
+                      controller: TextEditingController(
+                        text:
+                            "${selectedEndTime.hour}:${selectedEndTime.minute}",
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 25.0),
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromRGBO(51, 51, 51, 0.10), // Shadow color
+                    offset: Offset(0, 8), // Offset in the x, y direction
+                    blurRadius: 21.0,
+                    spreadRadius: 0.0,
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.white,
+              ),
+              child: TextFormField(
+                style: GoogleFonts.inter(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                ),
+                decoration: const InputDecoration(
+                  hintText: 'Seats',
+                  hintStyle: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xffADADAD),
+                  ),
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 20.0,
+                  ),
+                ),
+                onChanged: (String value) {
+                  seats = value;
+                },
+              ),
+            ),
+            const SizedBox(height: 25.0),
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: TextButton(
+                  // style: ElevatedButton.styleFrom(
+                  //   primary: const Color.fromRGBO(254, 114, 76, 0.70),
+                  //   shape: RoundedRectangleBorder(
+                  //     borderRadius: BorderRadius.circular(10.0),
+                  //   ),
+                  // ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const CabAddSuccess(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: const Color.fromRGBO(254, 114, 76, 0.70),
+                      boxShadow: const [
+                        BoxShadow(
+                          color:
+                              Color.fromRGBO(51, 51, 51, 0.10), // Shadow color
+                          offset: Offset(0, 8), // Offset in the x, y direction
+                          blurRadius: 21.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                    ),
+                    // color: const Color.fromRGBO(254, 114, 76, 0.70),
+                    width: double.infinity,
+                    height: 60,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Add Cab',
+                      style: GoogleFonts.inter(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 25.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
