2e8187da93947bdc94b77f45d1edcbef68ffdd89
Bhaskar <bhaskarmandal369@gmail.com>
2024-06-08 23:16:04 +0530

    firebase initialized

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:frontend/screens/home_screen.dart';
+import 'package:frontend/firebase_options.dart';
 import 'package:frontend/screens/login_screen.dart';
 import 'package:frontend/screens/splash_screen.dart';
 // import 'package:frontend/screens/login_screen.dart';
 // import 'package:frontend/screens/splash_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -19,7 +26,10 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(nextPage: LoginScreen(timeDilation: 4,)),
+      home: SplashScreen(
+          nextPage: LoginScreen(
+        timeDilation: 4,
+      )),
       // home: HomeScreen(user: 'guest'),
     );
   }
