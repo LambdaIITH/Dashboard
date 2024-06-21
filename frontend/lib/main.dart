fee9ff0e7124eb57ad94e2b30ee645c80232add8
Bhaskar <bhaskarmandal369@gmail.com>
2024-06-21 15:55:24 +0530

    add: json data and data parser

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/firebase_options.dart';
+import 'package:frontend/screens/bus_timings_screen.dart';
 import 'package:frontend/screens/home_screen.dart';
 // import 'package:frontend/screens/mess_menu_screen.dart';
 // import 'package:frontend/screens/login_screen.dart';
@@ -8,9 +9,9 @@ import 'package:frontend/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   runApp(const MyApp());
 }
 
@@ -30,7 +31,7 @@ class _MyAppState extends State<MyApp> {
       //     nextPage: LoginScreenWrapper(
       //   timeDilationFactor: 4.0,
       // )),
-      home: HomeScreen(user: ''),
+      home: BusTimingsScreen(),
       // home: MessMenuScreen(),
     );
   }
