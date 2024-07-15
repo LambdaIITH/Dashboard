03a9976c86e48602ca996adadc937b7d543c9e3d
adhitht <hello@adhitht.me>
2024-07-15 18:55:18 +0530

    feat: getRollNumber- get roll number from email

--- frontend/lib/models/user_model.dart
+++ frontend/lib/models/user_model.dart
@@ -5,6 +5,14 @@ class UserModel {
   String name;
   String? phone;
 
+  // a function to get Roll Number from email
+  // E.g., ma22btech11003@iith.ac.in -> MA22BTECH11003
+  String getRollNumber() {
+    List<String> parts = email.split('@');
+    String rollno = parts[0];
+    return rollno.toUpperCase();
+  }
+
   UserModel(
       {required this.email,
       this.cr = false,
