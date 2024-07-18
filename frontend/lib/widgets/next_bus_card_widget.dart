6ad0067122e6f5c20784f9e09fcf652f69404e84
adhitht <hello@adhitht.me>
2024-07-18 11:31:10 +0530

    fix: Overflow issue in bus card

--- frontend/lib/widgets/next_bus_card_widget.dart
+++ frontend/lib/widgets/next_bus_card_widget.dart
@@ -17,6 +17,11 @@ class NextBusCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    // TODO: Use Fractionally sized box later
+    // Calculates cardsize by taking screenWidth and subtracting padding
+    double cardWidth = screenWidth - 78;
     return Padding(
       padding: const EdgeInsets.fromLTRB(24, 0, 24, 0),
       child: Card(
@@ -31,87 +36,88 @@ class NextBusCard extends StatelessWidget {
           ),
           alignment: Alignment.centerLeft,
           width: double.infinity,
-          padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+          padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
           // height: 141.0,
           child: Stack(children: [
             Row(
               // mainAxisAlignment: MainAxisAlignment.spaceAround,spaceBetween
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 12.0),
-                    Text(
-                      'In next',
-                      style: GoogleFonts.inter(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w400,
-                          color: const Color(0xff6A6A6A),
-                          letterSpacing: -0.2),
-                    ),
-                    Text(
-                      waitingTime,
-                      style: GoogleFonts.inter(
-                          color: const Color(0xff6A6A6A),
-                          fontSize: 25.0,
-                          fontWeight: FontWeight.w500,
-                          letterSpacing: -0.2),
-                    ),
-                    const SizedBox(height: 6),
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Flexible(
-                          child: Text(
-                            from,
-                            style: GoogleFonts.inter(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w500,
-                              letterSpacing: -0.2,
-                              color: const Color(0xff6A6A6A),
+                SizedBox(
+                  width: cardWidth * 0.65,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 12.0),
+                      Text(
+                        'In next',
+                        style: GoogleFonts.inter(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w400,
+                            color: const Color(0xff6A6A6A),
+                            letterSpacing: -0.2),
+                      ),
+                      Text(
+                        waitingTime,
+                        style: GoogleFonts.inter(
+                            color: const Color(0xff6A6A6A),
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: -0.2),
+                      ),
+                      const SizedBox(height: 6),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Flexible(
+                            child: Text(
+                              from,
+                              style: GoogleFonts.inter(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: -0.2,
+                                color: const Color(0xff6A6A6A),
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
                             ),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
                           ),
-                        ),
-                        const SizedBox(width: 8),
-
-                        Container(
+                          const SizedBox(width: 8),
+                          Container(
                             margin: EdgeInsets.only(top: 2),
-                            child: SvgIcon("assets/icons/arrow.svg")),
-                        // const Icon(
-                        //   Icons.arrow_forward_rounded,
-                        //   size: 22,
-                        // ),
-                        const SizedBox(width: 8),
-                        Flexible(
-                          child: Text(
-                            destination,
-                            style: GoogleFonts.inter(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w500,
-                              letterSpacing: -0.2,
-                              color: const Color(0xff6A6A6A),
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
+                            child: SvgIcon("assets/icons/arrow.svg"),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 12),
-                  ],
+                          // const Icon(
+                          //   Icons.arrow_forward_rounded,
+                          //   size: 22,
+                          // ),
+                          const SizedBox(width: 8),
+                          Flexible(
+                            child: Text(
+                              destination,
+                              style: GoogleFonts.inter(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: -0.2,
+                                color: const Color(0xff6A6A6A),
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 12),
+                    ],
+                  ),
                 ),
-                Spacer(),
-                Container(
-                  width: 100,
+                const Spacer(),
+                SizedBox(
+                  width: cardWidth * 0.3,
                   child: Image.asset(
                       isEv ? "assets/icons/ev.png" : "assets/icons/bus.png"),
                 ),
-                const SizedBox(
-                  width: 8,
-                )
+                const SizedBox(width: 8,)
               ],
             ),
             Row(
