5643d9d36eeb4600bb8c49152017e5ce5302e85b
Bhaskar <bhaskarmandal369@gmail.com>
2024-10-23 12:46:13 +0530

    fix: clear data script

--- backend/backend/job_scripts/clear_lost_found_data_periodically.py
+++ backend/backend/job_scripts/clear_lost_found_data_periodically.py
@@ -1,6 +1,28 @@
 from psycopg2 import sql
 from datetime import datetime, timedelta
-from utils import conn
+from dotenv import load_dotenv
+import psycopg2
+import os
+
+load_dotenv()
+
+DATABASE = os.getenv("DATABASE")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASS = os.getenv("POSTGRES_PASS")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
+try: 
+    conn = psycopg2.connect(
+        database=DATABASE,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASS,
+        host=POSTGRES_HOST,
+        port=POSTGRES_PORT,
+    )
+    print("Connected Successfully!\n")
+except Exception as e:
+    print(f"Connection Failed: {e}")
 
 def delete_old_records():
     try:
